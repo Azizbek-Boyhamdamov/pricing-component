fae8d1f36b7f51c88dc8fd71b0928dd27f60fc8c
Azizbek-Boyhamdamov <boyhamdamovazizbek27@gmail.com>
2022-04-22 11:08:53 +0500

    Add swtich-case condition to main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,45 @@
-const elSwitchInputAnnualy = document.querySelector(".switch-control-annualy");
-const elPricingPlanValue = document.querySelector(".pricing-plan__value");
 
-elSwitchInputAnnualy.addEventListener("checked", function(){
-  elPricingPlanValue.textContent = 199.99;
+let basicPrice = document.querySelector('#basic-price');
+let professionalPrice = document.querySelector('#professional-price');
+let masterPrice = document.querySelector('#master-price');
+document.body.addEventListener('change', function(e){
+  let target = e.target;
+  let message;
+
+  // Basic-Price
+
+  switch (target.id){
+    case 'annual-payment-radio':
+      message = '199.99';
+      break;
+    case 'monthly-payment-radio':
+      message = '19.99';
+      break;
+  }
+  basicPrice.textContent = message;
+
+  // Professioanl Price
+
+  switch (target.id){
+    case 'annual-payment-radio':
+      message = '249.99';
+      break;
+    case 'monthly-payment-radio':
+      message = '24.99';
+      break;
+  }
+  professionalPrice.textContent = message;
+
+  // Master Price
+
+  switch (target.id){
+    case 'annual-payment-radio':
+      message = '399.99';
+      break;
+    case 'monthly-payment-radio':
+      message = '39.99';
+      break;
+  }
+  masterPrice.textContent = message;
 });
 
